1cc3df8201012bd67e84a0a554d3d33ccb83117a
Eduardo Lucio <eduardo.lucio@eisys.com.br>
2024-03-11 11:00:08 -0300

    Comit inicial - Scopo da estrutura da aplicação

---
+++ src/EiSys.GestaoDeEstoque.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EiSys.GestaoDeEstoque.WebUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
