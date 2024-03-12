81b0a96c1c022320ffc7b0f97556ebfe962e8fc9
Eduardo Lucio <eduardo.lucio@eisys.com.br>
2024-03-12 06:55:48 -0300

    - Criado as classes de Interface para Categoria e Produto - Criado as tasks de ICtegoria e IProduto

---
+++ src/EiSys.GestaoDeEstoque.Domain/Interface/ICategoriaRepository.cs
@@ -0,0 +1,24 @@
+﻿using EiSys.GestaoDeEstoque.Domain.Entities;
+
+
+namespace EiSys.GestaoDeEstoque.Domain.Interface
+{
+    internal interface ICategoriaRepository
+    {
+        //Task seria uma ação assincrona (não são em tempo de execução.
+        //Nas interfaces são assinaturas de metodos.
+        //O recomendado é que no final do metodo colocamos Async
+
+        //Pega uma lista de categorias
+        Task<IEnumerable<Categoria>> GetCategoriesAsync();
+
+        //Pega uma categoria
+        Task<Categoria> GetByIdAsync(int? id);
+
+
+        //passamos o objeto categoria do tipo Categoria
+        Task<Categoria> CreateAsync(Categoria categoria);
+        Task<Categoria> UpdateAsync(Categoria categoria);
+        Task<Categoria> RemoveAsync(Categoria categoria);
+    }
+}
---
+++ src/EiSys.GestaoDeEstoque.Domain/Interface/IProdutoRepository.cs
@@ -0,0 +1,24 @@
+﻿using EiSys.GestaoDeEstoque.Domain.Entities;
+
+namespace EiSys.GestaoDeEstoque.Domain.Interface
+{
+    public interface IProdutoRepository
+    {
+        //Task seria uma ação assincrona (não são em tempo de execução).
+        //Nas interfaces são assinaturas de metodos.
+
+        //Pega uma lista de Produtos
+        Task<IEnumerable<Produto>> GetProductAsync();
+
+        //Pega um Produto
+        Task<Produto> GetByIdAsync(int? id);
+
+        Task<Produto> GetProductCategoryAsync(int? id);
+
+
+        //passamos o objeto produto do tipo Produto
+        Task<Produto> CreateAsync(Produto produto);
+        Task<Produto> UpdateAsync(Produto produto);
+        Task<Produto> RemoveAsync(Produto produto);
+    }
+}
